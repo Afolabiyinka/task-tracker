68020b2e11905d82d156bbe573027e2a2c971714
Afolabiyinka <163763149+Afolabiyinka@users.noreply.github.com>
2025-04-26 15:12:59 +0100

    Updating our logo

--- src/Pages/Ad Pages/Pricing.jsx
+++ src/Pages/Ad Pages/Pricing.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import PricingCard from "../../Components/Basic Components/PricingCard";
 
 const Pricing = () => {
-  async function getPermissions() {
-    try {
-      // Camera & Mic
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true,
-      });
+  // async function getPermissions() {
+  //   try {
+  //     // Camera & Mic
+  //     const stream = await navigator.mediaDevices.getUserMedia({
+  //       video: true,
+  //       audio: true,
+  //     });
 
-      const notifications = await Notification.requestPermission();
-      notifications();
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  //     const notifications = await Notification.requestPermission();
+  //     notifications();
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-  getPermissions();
+  // getPermissions();
   return (
     <div className="h-full w-full flex flex-col md:flex-row justify-center items-center px-4 py-8">
       <PricingCard />
